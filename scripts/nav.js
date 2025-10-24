f2cc548756505beabaf5b8a0a7bf74730d8d2a26
kdtyktd <achdhdhd343738@gmail.com>
2025-10-24 16:13:49 +0300

    update

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,19 +1,32 @@
 const mediaQueryList = window.matchMedia("(max-width: 600px)");
 const navMenu = document.querySelector("#menubtn");
 const navContent = document.querySelector(".nav-link-ul");
+let isNavOpen = false;
+function showNav() {
+    if (!isNavOpen) {
+        navContent.classList.remove("d-none");
+        isNavOpen = true;
+    }
+    else {
+        navContent.classList.add("d-none");
+        isNavOpen = false;
+    }
+
+}
+
 if (mediaQueryList.matches) {
     navMenu.classList.remove('d-none');
     navContent.classList.add("d-none");
-    let isNavOpen = false;
-    navMenu.addEventListener("click", () => {
-        if (!isNavOpen) {
-            navContent.classList.remove("d-none");
-            isNavOpen = true;
-        }
-        else {
-            navContent.classList.add("d-none");
-            isNavOpen = false;
-        }
-
-    })
+    navMenu.addEventListener("click", showNav);
 }
+mediaQueryList.onchange = (e) => {
+    if (!(e.matches)) {
+        navContent.classList.remove("d-none");
+        navMenu.classList.add('d-none');
+        navMenu.removeEventListener("click", showNav);
+    } else {
+        navContent.classList.add("d-none");
+        navMenu.classList.remove('d-none');
+        navMenu.addEventListener("click", showNav);
+    }
+};
